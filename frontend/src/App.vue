7ba6366377e4cf242374a0dc90941c708568edec
Viktor Hultsten <viktor.hultsten@gmail.com>
2022-05-03 20:59:10 +0200

    changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,54 @@
 <template>
-  <p>matdagbok</p>
+  <div>
+    <input
+      type="text"
+      placeholder="Ingredient"
+      v-model="ingredient"
+      @keyup="checkIngredient"
+      list="ingredientsList"
+    />
+    <datalist id="ingredientsList">
+      <option
+        v-for="is in ingredientSuggestions"
+        :key="is"
+        :value="is.value"
+      ></option>
+    </datalist>
+    <input type="text" placeholder="Amount" v-model="ingredientAmount" />
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      ingredient: "",
+      ingredientSuggestions: [
+        { value: "wow" },
+        { value: "bigboy" },
+        { value: "pang" },
+      ],
+    };
+  },
+  created() {
+    fetch("http://localhost:3000/ingredients")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.ok) {
+          data.data.forEach((d) => {
+            this.ingredientSuggestions = [];
+            this.ingredientSuggestions.push({ value: d.name });
+          });
+        }
+      });
+  },
+  methods: {
+    checkIngredient() {
+      console.log("wow");
+    },
+  },
 };
 </script>
 
