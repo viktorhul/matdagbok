6c3ad1bd9f0d6d2081f7b442b67d04b9357a5a8d
Viktor Hultsten <viktor.hultsten@gmail.com>
2022-05-27 16:48:26 +0200

    Customize PWA settings

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: '../build'
+  outputDir: '../build',
+  pwa: {
+    name: 'Matdagboken',
+    themeColor: '#2e2e2e',
+    backgroundColor: '#2e2e2e',
+    appleMobileWebAppCapable: 'yes'
+  }
 })
